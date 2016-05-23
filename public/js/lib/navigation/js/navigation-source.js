8db4ed5cf95d2ec12be924629990b949d5bc2095
zhuyuanmei <625030221@qq.com>
2016-05-23 15:35:45 +0800

    add ajax for data
    add ajax for data

--- public/js/lib/navigation/js/navigation-source.js
+++ public/js/lib/navigation/js/navigation-source.js
@@ -5,6 +5,27 @@
  * @since 2016-05-09
  */
 define(function(require, exports, module) {
+    var Mustache = require('mustache');
+
+    var tpl = {
+        navigatorItemsCont:[
+            '{{#navigatorItems}}',
+                '<li class="base-info">',
+                    '<div class="base-info-lt">',
+                        '<img src="{{url}}">',
+                    '</div>',
+                    '<div class="base-info-rt">',
+                        '<div class="base-info-rt-hd">',
+                             '<div class="user-name"><span>{{name}}</span>{{post}}</div>',
+                         '</div>',
+                        '<div class="user-phone"><span class="hospital">{{hospital}}</span><span class="department">{{department}}</span></div>',
+                        '<div class="desc">简介：{{intro}}</div>',
+                    '</div>',
+                '</li>',
+            '{{/navigatorItems}}'
+        ]
+    };
+
     // 生成匹配namespace正则
     function matcherFor( ns ) {
         return new RegExp( '(?:^| )' + ns.replace( ' ', ' .* ?' ) + '(?: |$)' );
@@ -46,6 +67,23 @@ define(function(require, exports, module) {
 
         return this;
     }
+
+    var errorTip = function($target,tip){
+        $target.show();
+        $target.text(tip);
+
+        setTimeout(function(){
+            $target.removeClass('error-tip');
+            $target.addClass('error-tip-fade');
+
+            setTimeout(function(){
+                $target.removeClass('error-tip-fade');
+                $target.addClass('error-tip');
+
+                $target.hide();
+            },500);
+        },2000);
+    };
 
     var Navigation = function(options) {
         this.settings = $.extend({}, Navigation.defaults, options);
@@ -136,7 +174,6 @@ define(function(require, exports, module) {
 
                 _this.trigger( evt, list.get( to ) );
 
-
                 cur = list.removeClass( 'ui-state-active' )
                     .eq( to )
                     .addClass( 'ui-state-active' );
@@ -185,6 +222,53 @@ define(function(require, exports, module) {
                 }
 
                 _this.prevIndex = to;
+
+                //ajax请求对应分类下的数据     ??最好抽取出去
+                var $curItem = _this.$list.children().eq(to),
+                    curId = $curItem.attr('data-id'),
+                    curAjaxUrl = $curItem.attr('data-url'),
+                    curMaxPage = parseInt($curItem.attr('data-maxPage'));
+
+                opts.navItemContentObj.attr('data-curPage','2');
+
+                $.ajax({
+                    type: 'post',
+                    url: curAjaxUrl,
+                    data: {categoryId: curId, page: 1},
+                    dataType: 'json',
+                    success: function (res) {
+                        if(res.flag){
+                            if(res.navigatorItems.length){
+                                var navigatorItemsObj = {navigatorItems: res.navigatorItems};
+
+                                var resultStr = Mustache.render(tpl.navigatorItemsCont.join(''),navigatorItemsObj);
+
+                                opts.navItemContentObj.html(resultStr);
+
+                                if(curMaxPage <= 1){
+                                    opts.refreshDownObj.hide();
+
+                                    _this.disable('down', true);
+                                }else{
+                                    opts.refreshDownObj.show();
+
+                                    _this._setStyle('down','loaded');
+                                    _this.enable('down');
+                                }
+                            }else{
+                                opts.navItemContentObj.html('<div style="text-align:center;margin-top:80px;color:#A54093;font-size:20px;">暂无数据</div>');
+                                opts.refreshDownObj.hide();
+
+                                _this.disable('down', true);
+                            }
+                        }else{
+                            errorTip(opts.errorTipObj,res.msg);
+                        }
+                    },
+                    error: function (xhr, type) {
+                        errorTip(opts.errorTipObj,'请求服务器异常,稍后再试');
+                    }
+                });
             },
 
             /**
@@ -251,6 +335,204 @@ define(function(require, exports, module) {
             },
 
             /**
+             * 上拉刷新相关event
+             */
+            _changeStyle: function (dir, state) {
+                var opts = this.settings;
+
+                switch (state) {
+                    case 'loaded':
+                        opts.refreshLabelObj.html('加载更多');
+                        opts.refreshIconObj.removeClass();
+                        opts._actDir = '';
+                        break;
+                    case 'loading':
+                        opts.refreshLabelObj.html('加载中...');
+                        opts.refreshIconObj.addClass('ui-loading');
+                        opts._actDir = dir;
+                        break;
+                    case 'disable':
+                        opts.refreshLabelObj.html('没有更多内容了');
+                        break;
+                }
+
+                return this;
+            },
+
+            _setStyle: function (dir, state) {
+                var _this = this,
+                    stateChange = $.Event('statechange');
+
+                _this.trigger(stateChange, _this.settings['$' + dir + 'Elem'], state, dir);
+                if ( stateChange.defaultPrevented ) {
+                    return _this;
+                }
+
+                return _this._changeStyle(dir, state);
+            },
+
+            /**
+             * 用来设置加载是否可用，分方向的。
+             * @param {String} dir 加载的方向（'up' | 'down'）
+             * @param {String} status 状态（true | false）
+             */
+            _status: function(dir, status) {
+                var opts = this.settings;
+
+                return status === undefined ? opts['_' + dir + 'Open'] : opts['_' + dir + 'Open'] = !!status;
+            },
+
+            _setable: function (able, dir, hide) {
+                var _this = this,
+                    opts = _this.settings,
+                    dirArr = dir ? [dir] : ['up', 'down'];
+
+//                $.each(dirArr, function (i, dir) {
+                    var $elem = opts.refreshDownObj;
+                    if (!$elem.length) return;
+                    //若是enable操作，直接显示，disable则根据text是否是true来确定是否隐藏
+                    able ? $elem.show() : (hide ?  $elem.hide() : _this._setStyle(dir, 'disable'));
+                    _this._status(dir, able);
+//                });
+
+                return _this;
+            },
+
+            /**
+             * 如果已无类容可加载时，可以调用此方法来，禁用Refresh。
+             * @param {String} dir 加载的方向（'up' | 'down'）
+             * @param {Boolean} hide 是否隐藏按钮。如果此属性为false，将只有文字变化。
+             */
+            disable: function (dir, hide) {
+                return this._setable(false, dir, hide);
+            },
+
+            /**
+             * 启用组件
+             * @param {String} dir 加载的方向（'up' | 'down'）
+             */
+            enable: function (dir) {
+                return this._setable(true, dir);
+            },
+
+            _loadingAction: function (dir, type) {
+                var _this = this,
+                    opts = _this.settings,
+                    loadFn = _this.load;
+
+                $.isFunction(loadFn) && loadFn.call(_this, dir, type);
+                _this._status(dir, false);
+
+                return _this;
+            },
+
+            /**
+             * 当组件调用load，在load中通过ajax请求内容回来后，需要调用此方法，来改变refresh状态。
+             * @param {String} dir 加载的方向（'up' | 'down'）
+             */
+            afterDataLoading: function (dir) {
+                var _this = this,
+                    dir = dir || _this.settings._actDir;
+
+                _this._setStyle(dir, 'loaded');
+                _this._status(dir, true);
+
+                return _this;
+            },
+
+            load: function(dir, type){
+                var _this = this;
+
+                var $list = _this.settings.navItemContentObj;
+
+                if(parseInt($('li.ui-state-active').attr('data-maxPage')) >= parseInt($list.attr('data-curPage')) && dir === 'down'){
+                    $.ajax({
+                        type: 'post',
+                        url: $('li.ui-state-active').attr('data-url'),
+                        data: {categoryId: $('li.ui-state-active').attr('data-id'),page:parseInt($list.attr('data-curPage'))},
+                        dataType: 'json',
+                        success: function (res) {
+                            if(parseInt($('li.ui-state-active').attr('data-maxPage')) >= parseInt($list.attr('data-curPage'))){
+                                setTimeout(function(){
+                                    var resultHtml = Mustache.render(tpl.navigatorItemsCont.join(''),res);
+
+                                    $list['append'](resultHtml);
+                                    _this.afterDataLoading();    //数据加载完成后改变状态
+
+                                    $list.attr('data-curPage',parseInt($list.attr('data-curPage'))+1);
+
+                                    if(parseInt($list.attr('data-curPage')) > parseInt($('li.ui-state-active').attr('data-maxPage'))){
+                                        _this.disable('down', true);
+                                    }
+                                },500);
+                            }
+                        },
+                        error: function (xhr, type) {
+                            errorTip(_this.settings.errorTipObj,'请求服务器异常,稍后再试');
+                        }
+                    });
+                }
+            },
+
+            _startHandler: function (e) {
+                this.settings._startY = e.touches[0].pageY;
+            },
+
+            _endHandler: function () {
+                var _this = this,
+                    opts = _this.settings;
+                _this._setStyle('down', 'loading');
+
+                _this._loadingAction('down', 'pull');
+
+                opts['_refreshing'] = false;
+                return _this;
+            },
+
+            _moveHandler: function (e) {
+                var _this = this,
+                    opts = _this.settings,
+                    startY = opts._startY,
+                    movedY = startY - e.touches[0].pageY,
+                    winHeight = opts._win.innerHeight,
+                    threshold = opts.threshold || (opts.wrapperH < winHeight ? (opts.wrapperH / 2 + opts.wrapperTop || 0) : winHeight / 2);     //默认值为可视区域高度的一半，若wrapper高度不足屏幕一半时，则为list的一半
+
+                if (!_this._status('down') || movedY < 0) return;
+                if (!opts['_refreshing'] && (startY >= opts._body.scrollHeight - winHeight + threshold) && movedY > 10) {    //下边按钮，上拉加载
+                    _this._setStyle('down', 'beforeload');
+                    opts['_refreshing'] = true;
+                }
+                return _this;
+            },
+
+            _eventHandler: function (e) {
+                var _this = this,
+                    opts = _this.settings;
+
+                switch (e.type) {
+                    case 'touchstart':
+                        _this._startHandler(e);
+                        break;
+                    case 'touchmove':
+                        clearTimeout(opts._endTimer);        //解决部分android上，touchmove未禁用时，touchend不触发问题
+                        opts._endTimer = setTimeout( function () {
+                            _this._endHandler();
+                        }, 300);
+                        _this._moveHandler(e);
+                        break;
+                    case 'touchend':
+                    case 'touchcancel':
+                        clearTimeout(opts._endTimer);
+                        opts._refreshing && _this._endHandler();
+                        break;
+                    case 'scrollStop':
+                        (!opts._refreshing && opts._win.pageYOffset >= opts._body.scrollHeight - opts._win.innerHeight + (opts.threshold || -1)) && _this._endHandler();
+                        break;
+                }
+                return _this;
+            },
+
+            /**
              * 绑定函数
              */
             bindEvent: function(){
@@ -313,6 +595,19 @@ define(function(require, exports, module) {
                     $el.iScroll( 'destroy' );
                     $(window).off( 'resize');
                 });
+
+                //设置iScroll相关参数
+                $el.iScroll(opts.iScrollArg);
+
+                //定义往上拉动态刷新data事件
+                opts.pullAreaObj.on('touchstart touchmove touchend touchcancel', function(e){
+                    opts.wrapperH = opts.pullAreaObj.height();
+                    opts.wrapperTop = opts.pullAreaObj.offset().top;
+                    opts._win = window;
+                    opts._body = document.body;
+
+                    _this._eventHandler(e);
+                });
             }
         };
 
@@ -320,15 +615,34 @@ define(function(require, exports, module) {
      * 默认配置
      */
     Navigation.defaults = {
-        //导航元素对象
+        //导航列表父对象
         navListObj: null,
 
+        //导航项的详情列表对象
+        navItemContentObj: null,
+
+        //上拉区域载体对象
+        pullAreaObj: null,
+
+        //上拉bar区域对象
+        refreshDownObj: null,
+
+        //上拉bar区域-icon对象
+        refreshIconObj: null,
+
+        //上拉bar区域-label对象
+        refreshLabelObj: null,
+
+        //异常提示语载体对象
+        errorTipObj: null,
+
         //iScroll参数
-        iScroll: {
+        iScrollArg: {
             hScroll: true,
             vScroll: false,
             hScrollbar: false,
-            vScrollbar: false
+            vScrollbar: false,
+            bounce:false
         },
 
          //菜单数组内容
